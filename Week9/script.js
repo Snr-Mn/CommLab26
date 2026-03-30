25bd849a045bf77a91b5b614fffc1f0e6a7f72dd
Snr-Mn <sonorm971@gmail.com>
2026-03-30 17:35:44 +0800

    Week 9 hw unfinished

--- Week9/script.js
+++ Week9/script.js
@@ -12,11 +12,30 @@ let adSpace = document.querySelector('.tabScreen');
 
 function spawnAd() {
     document.querySelector(".skipAd").innerHTML = "<b>MORE ADS</b>";
-    const newAd = document.createElement('div');
+    const newAd = document.createElement('button');
     newAd.className = 'moreAds';
     newAd.style.position = 'absolute';
     newAd.style.left = Math.random() * 90 + '%';
     newAd.style.top = Math.random() * 90 + '%';
 
+    newAd.style.width = Math.random() * 300 + 'px';
+    newAd.style.height = Math.random() * 100 + 'px';
+    newAd.style.backgroundColor = getRandomRGBColor();
+
+
+    newAd.onclick = function () {
+        newAd.style.backgroundColor = 'transparent'
+        newAd.style.border = 'none'
+    };
+
+
     adSpace.append(newAd);
+}
+
+function getRandomRGBColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${green}, ${blue})`;
 }
